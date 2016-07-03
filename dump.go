fd728f540cd48239b02fd815232bbcf95c8ccb15
jumale <dev.yury.maltsev@gmail.com>
2016-07-03 17:46:55 +0200

    refactore code

--- dump.go
+++ dump.go
@@ -85,7 +85,7 @@ func Dump(args ...interface{}) {
 	_, fileName, fileLine, _ := runtime.Caller(1)
 
 	if Config.Location {
-		writeCF("%s:%d", Config.Color.Location, fileName, fileLine)
+		writeColor("%s:%d", Config.Color.Location, fileName, fileLine)
 		write("\n")
 	}
 
@@ -93,7 +93,7 @@ func Dump(args ...interface{}) {
 		dumpValue(arg, 1)
 		write("\n")
 	}
-	writeF("\033[%dm", FgDefault)
+	writeFormat("\033[%dm", FgDefault)
 }
 
 // By default, result goes to os.Stdout,
@@ -123,7 +123,7 @@ func dumpValue(val interface{}, level int) {
 	if kind == r.Ptr {
 		refVal = r.Indirect(refVal)
 		kind = refVal.Kind()
-		writeCF("%s", Config.Color.Punctuation, "&")
+		writeColor("%s", Config.Color.Punctuation, "&")
 	}
 
 	// Tabulations
@@ -139,19 +139,19 @@ func dumpValue(val interface{}, level int) {
 	switch kind {
 
 	case r.String:
-		writeCF("\"%s\"", Config.Color.String, refVal.String())
+		writeColor("\"%s\"", Config.Color.String, refVal.String())
 
 	case r.Int, r.Int8, r.Int16, r.Int32, r.Int64:
-		writeCF("%d", Config.Color.Number, refVal.Int())
+		writeColor("%d", Config.Color.Number, refVal.Int())
 
 	case r.Uint, r.Uint8, r.Uint16, r.Uint32, r.Uint64:
-		writeCF("%d", Config.Color.Number, refVal.Uint())
+		writeColor("%d", Config.Color.Number, refVal.Uint())
 
 	case r.Float32, r.Float64:
-		writeCF("%v", Config.Color.Number, refVal.Float())
+		writeColor("%v", Config.Color.Number, refVal.Float())
 
 	case r.Bool:
-		writeCF("%v", Config.Color.Bool, refVal.Bool())
+		writeColor("%v", Config.Color.Bool, refVal.Bool())
 
 	case r.Array, r.Slice:
 		writeType(refVal)
@@ -159,12 +159,12 @@ func dumpValue(val interface{}, level int) {
 		// Just show empty brace for empty value
 		len := refVal.Len()
 		if 0 == len {
-			writeCF("%s", Config.Color.Braces, "[]")
+			writeColor("%s", Config.Color.Braces, "[]")
 			break
 		}
 
 		// Open brace
-		writeCF("%s", Config.Color.Braces, "[\n"+startTab)
+		writeColor("%s", Config.Color.Braces, "[\n"+startTab)
 
 		// Print nested values
 		for i := 0; i < len; i++ {
@@ -174,77 +174,77 @@ func dumpValue(val interface{}, level int) {
 			if i < len-1 {
 				end += startTab
 			}
-			writeCF("%s", Config.Color.Punctuation, end)
+			writeColor("%s", Config.Color.Punctuation, end)
 		}
 
 		// Close brace
-		writeCF("%s", Config.Color.Braces, endTab+"]")
+		writeColor("%s", Config.Color.Braces, endTab+"]")
 
 	case r.Map:
 		writeType(refVal)
 
 		// Just show empty brace for empty value
 		if 0 == len(refVal.MapKeys()) {
-			writeCF("%s", Config.Color.Braces, "{}")
+			writeColor("%s", Config.Color.Braces, "{}")
 			break
 		}
 
 		// Open brace
-		writeCF("%s", Config.Color.Braces, "{\n"+startTab)
+		writeColor("%s", Config.Color.Braces, "{\n"+startTab)
 
 		// Print nested key:value's
 		len := refVal.Len()
 		for _, key := range refVal.MapKeys() {
 			dumpValue(key, level+1)
-			writeCF("%s", Config.Color.Punctuation, ": ")
+			writeColor("%s", Config.Color.Punctuation, ": ")
 			dumpValue(refVal.MapIndex(key), level+1)
 
 			end := ",\n"
 			if len--; len > 0 {
 				end += startTab
 			}
-			writeCF("%s", Config.Color.Punctuation, end)
+			writeColor("%s", Config.Color.Punctuation, end)
 		}
 
 		// Close brace
-		writeCF("%s", Config.Color.Braces, endTab+"}")
+		writeColor("%s", Config.Color.Braces, endTab+"}")
 
 	case r.Struct:
-		writeCF("%s", Config.Color.Type, pureType(valType.String()))
+		writeColor("%s", Config.Color.Type, pureType(valType.String()))
 
 		// Just show empty brace for empty value
 		if 0 == refVal.NumField() {
-			writeCF("%s", Config.Color.Braces, "{}")
+			writeColor("%s", Config.Color.Braces, "{}")
 			break
 		}
 
 		// Open brance
-		writeCF("%s", Config.Color.Braces, "{\n"+startTab)
+		writeColor("%s", Config.Color.Braces, "{\n"+startTab)
 
 		// Print nested fieldName:Value's
 		for i := 0; i < refVal.NumField(); i++ {
 			dumpValue(refVal.Type().Field(i).Name, level+1)
-			writeCF("%s", Config.Color.Punctuation, ": ")
+			writeColor("%s", Config.Color.Punctuation, ": ")
 			dumpValue(refVal.Field(i), level+1)
 
 			end := ",\n"
 			if i < refVal.NumField()-1 {
 				end += startTab
 			}
-			writeCF("%s", Config.Color.Punctuation, end)
+			writeColor("%s", Config.Color.Punctuation, end)
 		}
 
 		// Close brace
-		writeCF("%s", Config.Color.Braces, endTab+"}")
+		writeColor("%s", Config.Color.Braces, endTab+"}")
 
 	case r.Func:
-		writeCF("%s", Config.Color.Func, valType.String())
+		writeColor("%s", Config.Color.Func, valType.String())
 
 	case r.Interface:
 		dumpValue(refVal.Elem(), level)
 
 	default:
-		writeCF("%v", FgDefault, refVal)
+		writeColor("%v", FgDefault, refVal)
 	}
 
 }
@@ -261,17 +261,17 @@ func write(value string) {
 	writer.Write([]byte(value))
 }
 
-func writeF(format string, val ...interface{}) {
+func writeFormat(format string, val ...interface{}) {
 	write(fmt.Sprintf(format, val...))
 }
 
-func writeCF(format string, color uint, val ...interface{}) {
+func writeColor(format string, color uint, val ...interface{}) {
 	str := fmt.Sprintf(format, val...)
-	writeF("\033[%dm%s", color, str)
+	writeFormat("\033[%dm%s", color, str)
 }
 
 func writeType(v r.Value) {
-	writeCF("%s", Config.Color.Type, "<"+pureType(v.Type().String())+">")
+	writeColor("%s", Config.Color.Type, "<"+pureType(v.Type().String())+">")
 }
 
 var numbers = [12]r.Kind{
